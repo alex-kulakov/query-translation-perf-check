918a26e17f157f8c1de262b1b80ad549c8c513d1
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2024-05-01 12:33:28 +0500

    Add scripts that help to run projects as batch
    each project will run 10 times with 5 seconds delay between runs

---
+++ runDO71benchmarks.ps1
@@ -0,0 +1,84 @@
+﻿dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet5\bin\Release\net5.0\DO71OnNet5.dll
+Start-Sleep -Seconds 5
+
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet6\bin\Release\net6.0\DO71OnNet6.dll
+Start-Sleep -Seconds 5
+
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet7\bin\Release\net7.0\DO71OnNet7.dll
+Start-Sleep -Seconds 5
+
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO71OnNet8\bin\Release\net8.0\DO71OnNet8.dll
+
+pause
---
+++ runDO7benchmarks.ps1
@@ -0,0 +1,104 @@
+﻿dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet8\bin\Release\net8.0\DO7OnNet8.dll
+Start-Sleep -Seconds 5
+
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet7\bin\Release\net7.0\DO7OnNet7.dll
+Start-Sleep -Seconds 5
+
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet6\bin\Release\net6.0\DO7OnNet6.dll
+Start-Sleep -Seconds 5
+
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnNet5\bin\Release\net5.0\DO7OnNet5.dll
+Start-Sleep -Seconds 5
+
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+Start-Sleep -Seconds 5
+dotnet F:\Projects\DataObjects.Net\OtherStuff\QueryTranslationPerfCheck\DO7OnCore31\bin\Release\netcoreapp3.1\DO7OnCore31.dll
+pause
